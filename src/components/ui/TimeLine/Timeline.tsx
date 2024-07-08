1228033f8bb5cabeb8a1d3b6ae378735ac5eb95a
jdsuarez <jdsuarez@fgs.co>
2024-07-07 19:27:50 -0500

    fix: solving the bug

--- src/components/ui/TimeLine/Timeline.tsx
+++ src/components/ui/TimeLine/Timeline.tsx
@@ -1,13 +1,28 @@
+'use client';
 import { EXPERIENCE } from "@/constants";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Chrono } from "react-chrono";
 
-
 const Experience = () => {
-  return (
-     <Chrono items={EXPERIENCE} mode="VERTICAL_ALTERNATING"  slideItemDuration={5000} slideShow  />
+  const [isClient, setIsClient] = useState(false);
 
+  useEffect(() => {
+    // Esto se ejecutará solo en el cliente
+    setIsClient(true);
+  }, []);
+
+  return (
+    <>
+      {isClient && (
+        <Chrono
+          items={EXPERIENCE}
+          mode="VERTICAL_ALTERNATING"
+          slideItemDuration={5000}
+          slideShow
+        />
+      )}
+    </>
   );
 };
-export default   Experience;
 
+export default Experience;
